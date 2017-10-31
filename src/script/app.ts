4e72051b1bd55282eef5483c7718fc82d78b0db6
Nate Geslin <ngeslin@nerdery.com>
2017-10-30 19:31:41 -0500

    Initialize

---
+++ src/script/app.ts
@@ -0,0 +1,34 @@
+import StageViewController from './stage/StageViewController';
+
+class App {
+    public stageViewController: StageViewController = null;
+
+    constructor() {
+        this.init();
+    }
+
+    public init(): this {
+        return this.setupHandlers()
+            .createChildren()
+            .enable();
+    }
+
+    public setupHandlers(): this {
+        return this;
+    }
+
+    public createChildren(): this {
+        const element: SVGElement = document.getElementsByClassName('js-stage')[0] as SVGElement;
+        this.stageViewController = new StageViewController(element);
+
+        return this;
+    }
+
+    public enable(): this {
+        return this;
+    }
+}
+
+const app = new App();
+console.log(app);
+
---
+++ src/script/stage/StageViewController.ts
@@ -0,0 +1,9 @@
+class StageViewController {
+    private _element: SVGElement;
+
+    constructor(element: SVGElement) {
+        this._element = element;
+    }
+}
+
+export default StageViewController;
